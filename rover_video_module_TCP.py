c03d4ce1470e37436a16d58eec5c75bd86668f7b
raspberryrobot <56299826+raspberryrobot@users.noreply.github.com>
2019-10-10 09:00:56 -0400

    Update rover_video_module_TCP.py

--- rover_video_module_TCP.py
+++ rover_video_module_TCP.py
@@ -6,11 +6,12 @@
 import socket
 import time
 import picamera
+import sys
 
 # -----------------------------------------------------------------------------
 # Global definitions
 # -----------------------------------------------------------------------------
-DEBUG = False
+DEBUG = True
 VIDEO_RESOLUTION = (640,480)
 VIDEO_FRAMERATE = 24
 VIDEO_FORMAT = 'h264'
@@ -23,24 +24,26 @@ SERVER_PORT = 10004
 # -----------------------------------------------------------------------------
 def start_video_server():
     if DEBUG: print("[MSG]> start video server")
-    with picamera.PiCamera() as camera:
-        # Camera settings
-        camera.resolution = VIDEO_RESOLUTION
-        camera.framerate = VIDEO_FRAMERATE
-
-        # Open listening TCP socket
-        server_socket = socket.socket()
+    
+    # Open listening TCP socket
+    with socket.socket() as server_socket:
         server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
         server_socket.bind((SERVER_IP, SERVER_PORT))
         server_socket.listen(0)
         if DEBUG: print("[MSG]> listening on %s:%d" % (SERVER_IP, SERVER_PORT))
-        
-        # Client is connected       
+
+        # Client is connected
         (client_socket, client_address) = server_socket.accept()
         if DEBUG: print("[MSG]> connection from %s:%d" % (client_address))
         connection = client_socket.makefile('wb')
 
-        # Start video streaming
+    # Open camera device
+    with picamera.PiCamera() as camera:
+        # Set camera configuration
+        camera.resolution = VIDEO_RESOLUTION
+        camera.framerate = VIDEO_FRAMERATE
+        
+        # Start camera streaming
         try:
             if DEBUG: print("[MSG]> start video capture")
             camera.start_preview()
@@ -48,15 +51,8 @@ def start_video_server():
             camera.start_recording(connection, format=VIDEO_FORMAT)
             while True:
                 camera.wait_recording(VIDEO_LENGTH)
-            camera.stop_recording()
-            if DEBUG: print("[MSG]> stop video capture")
-        except:
-            if DEBUG: print("[MSG]> error: exception")
-            camera.stop_recording()
         finally:
-            if DEBUG: print("[MSG]> close network socket")
-            connection.close()
-            server_socket.close()
+            camera.stop_recording()
 
 # -----------------------------------------------------------------------------
 # Main
@@ -65,6 +61,9 @@ if __name__ == "__main__":
     while 1:
         try:
             start_video_server()
+        except KeyboardInterrupt:
+            print("\r\n[MSG]> CTRL-C - keyboard interupt")
+            sys.exit(0)
         except BaseException as e:
             if DEBUG: print("[MSG]> error: exception %s" % e)
         time.sleep(1)
